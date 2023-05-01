081f32d20f854cbabcb8a84e22e35c423f06f753
satyam gupta <satyamgupta2997@gmail.com>
2023-05-01 23:10:21 +0530

    rooms/room list ui creation

--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -1,53 +1,59 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { logout } from "../../../http";
-import { setAuth } from "../../../store/authSlice";
-import styles from "./Navigation.module.css";
+import React from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+import { logout } from "../../../http"
+import { setAuth } from "../../../store/authSlice"
+import styles from "./Navigation.module.css"
 
 const Navigation = () => {
-    const dispatch = useDispatch();
-    const {isAuth, user} = useSelector(state => state.auth)
-    const brandStyle = {
-        color: "#fff",
-        textDecoration: "none",
-        fontWeight: "bold",
-        fontSize: "22px",
-        display: "flex",
-        alignItems: "center",
-    };
-    const logoText = {
-        marginLeft: "10px",
-    };
+  const dispatch = useDispatch()
+  const { isAuth, user } = useSelector((state) => state.auth)
+  const brandStyle = {
+    color: "#fff",
+    textDecoration: "none",
+    fontWeight: "bold",
+    fontSize: "22px",
+    display: "flex",
+    alignItems: "center",
+  }
+  const logoText = {
+    marginLeft: "10px",
+  }
 
-    const handleLogout = async () => {
-        try {
-            const {data} = await logout()
-            dispatch(setAuth(data))
-        } catch (error) {
-            console.log(error, 'logout')
-        }
+  const handleLogout = async () => {
+    try {
+      const { data } = await logout()
+      dispatch(setAuth(data))
+    } catch (error) {
+      console.log(error, "logout")
     }
+  }
 
-    return (
-        <nav className={`${styles.navbar} container`}>
-            <Link style={brandStyle} to="/">
-                <img src="/images/logo.png" alt="logo" />
-                <span style={logoText}>Coder's House</span>
-            </Link>
-            <div className={styles.navRight}>
-                <h3>{user.name}</h3>
-                {/* to direct to profile section */}
-                <Link to='/'>
-                    <img src={user.avatar} className={styles.avatar} width="30" height="30" alt="avatar" />
-                </Link>
-                <button onClick={handleLogout} className={styles.logoutButton}>
-                    <img src="/images/logout.png" alt="Logout" />
-                </button>
-            </div>
-            {/* {isAuth && <button onClick={handleLogout}>Logout</button>} */}
-        </nav>
-    );
-};
+  return (
+    <nav className={`${styles.navbar} container`}>
+      <Link style={brandStyle} to="/">
+        <img src="/images/logo.png" alt="logo" />
+        <span style={logoText}>Coder's House</span>
+      </Link>
+      <div className={styles.navRight}>
+        <h3>{user.name}</h3>
+        {/* to direct to profile section */}
+        <Link to="/">
+          <img
+            src={user.avatar}
+            className={styles.avatar}
+            width="30"
+            height="30"
+            alt="avatar"
+          />
+        </Link>
+        <button onClick={handleLogout} className={styles.logoutButton}>
+          <img src="/images/logout.png" alt="Logout" />
+        </button>
+      </div>
+      {/* {isAuth && <button onClick={handleLogout}>Logout</button>} */}
+    </nav>
+  )
+}
 
-export default Navigation;
+export default Navigation
--- frontend/src/pages/Rooms/Rooms.jsx
+++ frontend/src/pages/Rooms/Rooms.jsx
@@ -1,8 +1,138 @@
-import React from 'react'
+import React from "react"
+import styles from "./Rooms.module.css"
+import RoomCard from "../../components/RoomCard/RoomCard"
 
 const Rooms = () => {
+  const rooms = [
+    {
+      id: 1,
+      topic: "Which framework is best?",
+      speakers: [
+        {
+          id: 1,
+          name: "Jhone Doe",
+          avatar: "/images/monkey-avatar.png",
+        },
+        {
+          id: 2,
+          name: "Heelo Jack",
+          avatar: "/images/monkey-avatar.png",
+        },
+      ],
+      totalPeople: 40,
+    },
+    {
+      id: 2,
+      topic: "Which framework is best?",
+      speakers: [
+        {
+          id: 1,
+          name: "Jhone Doe",
+          avatar: "/images/monkey-avatar.png",
+        },
+        {
+          id: 2,
+          name: "Heelo Jack",
+          avatar: "/images/monkey-avatar.png",
+        },
+      ],
+      totalPeople: 40,
+    },
+    {
+      id: 3,
+      topic: "Which framework is best?",
+      speakers: [
+        {
+          id: 1,
+          name: "Jhone Doe",
+          avatar: "/images/monkey-avatar.png",
+        },
+        {
+          id: 2,
+          name: "Heelo Jack",
+          avatar: "/images/monkey-avatar.png",
+        },
+      ],
+      totalPeople: 40,
+    },
+    {
+      id: 4,
+      topic: "Which framework is best?",
+      speakers: [
+        {
+          id: 1,
+          name: "Jhone Doe",
+          avatar: "/images/monkey-avatar.png",
+        },
+        {
+          id: 2,
+          name: "Heelo Jack",
+          avatar: "/images/monkey-avatar.png",
+        },
+      ],
+      totalPeople: 40,
+    },
+    {
+      id: 5,
+      topic: "Which framework is best?",
+      speakers: [
+        {
+          id: 1,
+          name: "Jhone Doe",
+          avatar: "/images/monkey-avatar.png",
+        },
+        {
+          id: 2,
+          name: "Heelo Jack",
+          avatar: "/images/monkey-avatar.png",
+        },
+      ],
+      totalPeople: 40,
+    },
+    {
+      id: 6,
+      topic: "Which framework is best?",
+      speakers: [
+        {
+          id: 1,
+          name: "Jhone Doe",
+          avatar: "/images/monkey-avatar.png",
+        },
+        {
+          id: 2,
+          name: "Heelo Jack",
+          avatar: "/images/monkey-avatar.png",
+        },
+      ],
+      totalPeople: 40,
+    },
+  ]
+
   return (
-    <div>Rooms</div>
+    <>
+      <div className="container">
+        <div className={styles.roomsHeader}>
+          <div className={styles.left}>
+            <span className={styles.heading}>All voice rooms</span>
+            <div className={styles.searchBox}>
+              <img src="/images/search-icon.png" alt="search" />
+              <input className={styles.searchInput} type="text" />
+            </div>
+          </div>
+          <div className={styles.right}>
+            <button className={styles.startRoomButton}>
+              <img src="/images/add-room-icon.png" alt="add room" />
+              <span className={styles.startRoomText}>Start a room</span>
+            </button>
+          </div>
+        </div>
+        <div className={styles.roomList}>
+          {rooms.map((room) => {
+            return <RoomCard key={room.id} room={room} />
+          })}
+        </div>
+      </div>
+    </>
   )
 }
 
